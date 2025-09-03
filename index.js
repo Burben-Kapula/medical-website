40ea9962db3a2aa474880c579c0f6a4d71c9467d
Burben-Kapula <burbenburbenov@gmail.com>
2025-09-03 11:56:55 +0300

    03.09.2025 null

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ const camera = new THREE.OrthographicCamera(
   1000
 );
 
-
-// scene.background = new THREE.Color(0xffffff); // <-- білий фон
+//тут бек граунд
+scene.background = new THREE.Color(0xffffff); // <-- білий фон
 
 camera.position.set(1, 0, 80);
 camera.lookAt(0, 0, 0);
